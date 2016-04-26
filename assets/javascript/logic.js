c9ae69ef023c0fe8305d13e38c5f547ee0d72919
Ksenia Cruey <Joshua@crueyphotography.com>
2016-04-26 08:22:54 -0400

    added composer.json and index.php

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -47,7 +47,7 @@ var game = {
 
     // function to create character buttons
     charList: function() {   
-    	for (var i = 0; i < game.characters.length; i++) {
+    	for (var i = 0; i < this.characters.length; i++) {
 
         var c = $('<button>');
         c.addClass("selChar " + game.characters[i].name);
@@ -86,12 +86,14 @@ var game = {
     $('#Enemies').on('click', 'button', function() {
     		if (game.clickEnemy <= 0 || game.enemyHealth <= 0) {
     			$(this).removeClass("Enemies").addClass("Defender");
-				$('.Defender').appendTo('#Defender');
 				$(this).attr('id', "chosenEnemy");
 				game.chosenEnemy = $(this).data("name");
 				game.enemyHealth = $(this).data("health");
 				game.enemyAttack = $(this).data("counterattack");
 				game.clickEnemy	 ++;
+				$('Defender').empty();
+				$('.Defender').appendTo('#Defender');
+				$('#results').empty();
 			} else {
 				$('#results').html("You've already chosen " + game.chosenEnemy);
 			}
@@ -100,24 +102,26 @@ var game = {
 	//Attack button function
     $('#buttonAttack').on('click', function() {	
 	
-		if (game.charHealth && game.enemyHealth >= 0) {
+		if (game.enemyCount == 0 && game.clickChar >= 1) {
+			$('#results').html("You defeated all the Enemies. You Win!! ");
+			game.clickChar = 0;
+			console.log(game.enemyCount);
+			$('#buttonNewGame').show();
+		} else if (game.clickEnemy == 0) {
+			$('#results').html("You need to select an Enemy to attack");
+		} else if (game.enemyHealth <= 0) {
+			$('#results').html("You defeated " + game.chosenEnemy);	
+			game.clickEnemy --;
+			game.enemyCount --;
+			$('#Defender').empty();
+		} else if (game.charHealth && game.enemyHealth >= 1) {
 			game.enemyHealth -= game.charAttack;			
 			game.charHealth -= game.enemyAttack;
-			console.log(game.enemyHealth);
-			console.log(game.charHealth);
 			$('#chosenChar h4 ').text(game.charHealth);
 			$('#chosenEnemy h4 ').text(game.enemyHealth);
 			$('#results').html("You attacked " + game.chosenEnemy + " for " + game.charAttack + " points! ");
 			$('#results').append( game.chosenEnemy + " counter attacked you for " + game.enemyAttack + " points");
 			game.charAttack += 8;
-		} else if (game.enemyCount == 0) {
-			$('#results').html("You defeated all the Enemies. You Win!! ");
-			game.clickChar = 0;
-			$('#buttonNewGame').show();
-		} else if (game.charHealth >= 1) {
-			$('#results').html("You defeated " + game.chosenEnemy);
-			game.enemyCount --;
-			$('#Defender').empty();
 		} else {
 			$('#results').html("You were defeated by " + game.chosenEnemy + ". Game Over!");
 			game.clickChar = 0;
@@ -125,6 +129,7 @@ var game = {
 		}
 	});
 
+	
 	//New Game function
 	$('#buttonNewGame').on('click', function() {
 		$('#results').empty();
@@ -132,6 +137,7 @@ var game = {
 		$('#Enemies').empty();
 		$('#selChar').empty();
 		$('#selCharpre').empty();
+		console.log(game.clickChar);
 		game.charList();
 
 	});	
